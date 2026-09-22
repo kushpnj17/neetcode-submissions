c6c2e115e245744f9d900630cc73023f894b58d6
kushpnj17 <133170036+kushpnj17@users.noreply.github.com>
2026-09-22 14:09:09 -0700

    Add: permutation-string - submission-26

---
+++ Data Structures & Algorithms/permutation-string/submission-26.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s2.size() < s1.size()) return false;
+
+        vector<int> s1_counts(26,0);
+        for(char c : s1){
+            s1_counts[c-'a']++;
+        }
+
+        vector<int> s2_counts(26, 0);
+        for(int i = 0; i < s1.size(); i++){
+            s2_counts[s2[i]-'a']++;
+        }
+
+        int matches = 0;
+        for(int i = 0; i < 26; i++){
+            if(s1_counts[i] == s2_counts[i]) matches++;
+        }
+
+        int l = 0;
+        for(int r = s1.size(); r < s2.size(); r++){
+            if(matches == 26) return true;
+
+            s2_counts[s2[l]-'a']--;
+            s2_counts[s2[r]-'a']++;
+            matches = 0;
+            for(int i = 0; i < 26; i++){
+                if(s1_counts[i] == s2_counts[i]) matches++;
+            }
+
+            l++;
+        }
+
+        return matches == 26;
+    }
+};
+
+// s1: ab
+// s2: abc
+// r: 2
+// l: 0
+
+
+
+
