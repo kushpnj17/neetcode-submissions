acdd4d0d76242c23088c600011c7b18ffe4e2268
kushpnj17 <133170036+kushpnj17@users.noreply.github.com>
2026-09-22 17:29:23 -0700

    Add: car-fleet - submission-10

---
+++ Data Structures & Algorithms/car-fleet/submission-10.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>> arr;
+        for(int i = 0; i < position.size(); i++){
+            arr.push_back({position[i], speed[i]});
+        }
+
+        sort(arr.begin(), arr.end());
+
+        int fleets = 0;
+        double highest = INT_MIN;
+        for(int i = arr.size()-1; i >= 0; i--){
+            double hrs = (target - arr[i].first) / static_cast<double>(arr[i].second);
+            if(hrs > highest){
+                fleets++;
+                highest = hrs;
+            }
+        }
+
+        return fleets;
+    }
+};
